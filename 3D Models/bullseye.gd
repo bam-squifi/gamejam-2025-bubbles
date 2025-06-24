187b1f5103f96c5a302680bab4bf2f21576fbac2
bam-squifi <81431370+bam-squifi@users.noreply.github.com>
2025-06-24 22:25:20 +0200

    macht schon laune, ist noch nicht fertig, aber ist schon witzig.

--- 3D Models/bullseye.gd
+++ 3D Models/bullseye.gd
@@ -1,12 +1,11 @@
 extends Node3D
 
+var current = 0
+var next = 0
+
 func _ready() -> void:
 	%Bullseye.visible = true
-	hide_segment(1)
-	var fall_timer = Timer.new()
-	
-func _process(delta: float) -> void:
-	%Bullseye.get_child(11).position.y -= 0.1
+	current = randi() % 12
 	
 func hide_segment(seg_id: int) -> void:
 	var mitte = %Bullseye.get_child(seg_id)
@@ -16,3 +15,12 @@ func hide_segment(seg_id: int) -> void:
 	var mitte_collision_body = mitte_static_body.get_child(0)
 	mitte_collision_body.disabled = true
 	
+func _on_timer_timeout() -> void:
+	%Bullseye.get_child(current).position.y -= 1
+	
+func _on_new_tile_timer_timeout() -> void:
+	# Ist noch sehr billig
+	# TODO: Feld muss aus der letzte der Kinder genommen werden und dann an die randi() range übergeben werden
+	# Timer wann das nächste Feld fällt. ;)
+	# Starte erst wenn Groundplane Timer weg ist
+	current = randi() % 11
